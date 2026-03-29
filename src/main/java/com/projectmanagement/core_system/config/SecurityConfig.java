c95d522d716057e5b5217f046a8c74a204e8c3e1
newsun48 <savanjnz2@gmai.com>
2026-03-29 19:59:24 +0700

    fix index và securityconf

--- src/main/java/com/projectmanagement/core_system/config/SecurityConfig.java
+++ src/main/java/com/projectmanagement/core_system/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
             "http://localhost:5173", 
             "http://127.0.0.1:5173", 
             "http://localhost:5174", 
-            "http://127.0.0.1:5174"
+            "http://127.0.0.1:5174",
+            "https://deployquanliduan-production.up.railway.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
